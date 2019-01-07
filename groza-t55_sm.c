8f5c0b3be26e9aefb2eb24f9820a241a31122b3d
Бажен Левковець <bazhen.paseka@gmail.com>
2019-01-07 20:55:06 +0200

    ref

--- groza-t55_sm.c
+++ groza-t55_sm.c
@@ -5,12 +5,12 @@ extern UART_HandleTypeDef huart1;
 extern TIM_HandleTypeDef htim4;
 
   char DataChar[100];
-  volatile uint32_t tim_k0;
-  volatile uint32_t tim_k1;
-  volatile uint32_t tim_k2;
-  volatile uint32_t tim_k3;
 
-  int value[4];
+  uint32_t timer_u32[4];
+  uint32_t channel_1_value_u32[4];
+  uint32_t channel_2_value_u32[4];
+
+  int value_i32[4];
 
   uint8_t flag_60_sec_u8 = 0;
 
@@ -23,52 +23,56 @@ void Groza_t55_init (void)
 
 void Groza_t55_main (void)
 {
-	tim_k0 = 1;
-	tim_k1 = 1;
-	tim_k2 = 1;
-	tim_k3 = 1;
+	for (int j=0; j<4; j++)
+	{
+		timer_u32[j] = 0;
+	}
 
-	  HAL_GPIO_WritePin(STROB14_GPIO_Port, STROB14_Pin, SET);
-	  for (int w=0; w<10; w++)
-	  	  { __asm("nop");}
-	  HAL_GPIO_WritePin(STROB14_GPIO_Port, STROB14_Pin, RESET);
+	HAL_GPIO_WritePin(STROB14_GPIO_Port, STROB14_Pin, SET);
+	for (int w=0; w<10; w++)
+	  { __asm("nop");}
+	HAL_GPIO_WritePin(STROB14_GPIO_Port, STROB14_Pin, RESET);
 
-	  TIM4->CNT = 0;
-	  HAL_TIM_Base_Start(&htim4);
-	  HAL_Delay(500);
-	  HAL_TIM_Base_Stop(&htim4);
+	TIM4->CNT = 0;
+	HAL_TIM_Base_Start(&htim4);
+	HAL_Delay(500);
+	HAL_TIM_Base_Stop(&htim4);
 
-	sprintf(DataChar,"%d (%d)  %d (%d)  ", (int)tim_k0, (int)(tim_k0-tim_k1), (int)tim_k2, (int)(tim_k3-tim_k2) );
+	value_i32[1] = (int)(timer_u32[0] - timer_u32[1]);
+	value_i32[2] = (int)(timer_u32[3] - timer_u32[2]);
+
+	sprintf(DataChar,"%d (%d)  %d (%d)  ", (int)timer_u32[0], value_i32[1], (int)timer_u32[2], value_i32[2] );
 	HAL_UART_Transmit(&huart1, (uint8_t *)DataChar, strlen(DataChar), 100);
-	value[1] = (int)(tim_k0-tim_k1);
-	value[2] = (int)(tim_k3-tim_k2);
 
-	tim_k0 = 1;
-	tim_k1 = 1;
-	tim_k2 = 1;
-	tim_k3 = 1;
 
-	  HAL_GPIO_WritePin(STROB15_GPIO_Port, STROB15_Pin, SET);
-	  for (int w=0; w<10; w++)
-	  	  { __asm("nop");}
-	  HAL_GPIO_WritePin(STROB15_GPIO_Port, STROB15_Pin, RESET);
 
-	  TIM4->CNT = 0;
-	  HAL_TIM_Base_Start(&htim4);
-	  HAL_Delay(500);
-	  HAL_TIM_Base_Stop(&htim4);
+	for (int j=0; j<4; j++)
+	{
+		timer_u32[j] = 0;
+	}
 
-	sprintf(DataChar,"%d (%d)  %d (%d)\r\n", (int)tim_k0, (int)(tim_k0-tim_k1), (int)tim_k2, (int)(tim_k2-tim_k3) );
+	HAL_GPIO_WritePin(STROB15_GPIO_Port, STROB15_Pin, SET);
+	for (int w=0; w<10; w++)
+	  { __asm("nop");}
+	HAL_GPIO_WritePin(STROB15_GPIO_Port, STROB15_Pin, RESET);
+
+	TIM4->CNT = 0;
+	HAL_TIM_Base_Start(&htim4);
+	HAL_Delay(500);
+	HAL_TIM_Base_Stop(&htim4);
+
+	value_i32[3] = (int)(timer_u32[0]-timer_u32[1]);
+	value_i32[4] = (int)(timer_u32[2]-timer_u32[3]);
+
+	sprintf(DataChar,"%d (%d)  %d (%d)\r\n", (int)timer_u32[0], value_i32[3], (int)timer_u32[2], value_i32[4] );
 	HAL_UART_Transmit(&huart1, (uint8_t *)DataChar, strlen(DataChar), 100);
-	value[3] = (int)(tim_k0-tim_k1);
-	value[4] = (int)(tim_k2-tim_k3);
+
 }
 //*****************************************************************************
-uint32_t Value_T55 (uint8_t _kanal)
+int Value_T55 (uint8_t _kanal)
 {
-	return value[_kanal];
+	return value_i32[_kanal];
 }
-
 
 //*****************************************************************************
 void Set_Flag_60_Sec(uint8_t _flag)
@@ -80,3 +84,10 @@ uint8_t Get_Flag_60_Sec(void)
 {
 	return flag_60_sec_u8;
 }
+//*****************************************************************************
+
+void Timer_Update( uint8_t _timer_u8, uint32_t _tim_value_u32)
+{
+	timer_u32[_timer_u8] = _tim_value_u32;
+}
+//*****************************************************************************
