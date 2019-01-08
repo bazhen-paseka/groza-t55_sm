53178691496fb73f824b353dfc8b574231a82d9c
Бажен Левковець <bazhen.paseka@gmail.com>
2019-01-08 15:11:58 +0200

    calc aver
    ok
    
    
    .

--- groza-t55_sm.c
+++ groza-t55_sm.c
@@ -13,7 +13,7 @@ extern ADC_HandleTypeDef hadc1;
   uint32_t channel_1_value_u32[4];
   uint32_t channel_2_value_u32[4];
 
-  int value_i32[4];
+  static uint32_t main_value_u32[DEVICE_QNT][CIRCLE_QNT];
 
   uint8_t flag_60_sec_u8 = 0;
 
@@ -21,18 +21,16 @@ void Groza_t55_init (void)
 {
 	sprintf(DataChar,"\r\n19ZH36 GROZA-T55 debug base time\r\nUART1 for debug started\r\nSpeed 38400\r\n");
 	HAL_UART_Transmit(&huart1, (uint8_t *)DataChar, strlen(DataChar), 100);
-
-	#define AVER_QNT 	7
-	uint32_t aver[AVER_QNT] = {60, 50, 40, 30, 20, 10, 0};
-	uint32_t aver_res = Calc_Average(aver, AVER_QNT);
-
-	sprintf(DataChar,"aver_res = %d\r\n", (int)aver_res);
-	HAL_UART_Transmit(&huart1, (uint8_t *)DataChar, strlen(DataChar), 100);
 }
 //*****************************************************************************
 
-void Groza_t55_main (char* http_req_1, uint8_t* size_of_http_req )
+void Groza_t55_main (uint8_t circle, char* http_req_1 )
 {
+	uint32_t value_i32[4];
+
+	sprintf(DataChar,"crc %d) ", (int)circle);
+	HAL_UART_Transmit(&huart1, (uint8_t *)DataChar, strlen(DataChar), 100);
+
 	for (int j=0; j<4; j++)
 	{
 		timer_u32[j] = 0;
@@ -51,7 +49,7 @@ void Groza_t55_main (char* http_req_1, uint8_t* size_of_http_req )
 	value_i32[1] = (int)(timer_u32[0] - timer_u32[1]);
 	value_i32[2] = (int)(timer_u32[3] - timer_u32[2]);
 
-	sprintf(DataChar,"%d (%d)  %d (%d)  ", (int)timer_u32[0], value_i32[1], (int)timer_u32[2], value_i32[2] );
+	sprintf(DataChar,"%d (%d)  %d (%d)  ", (int)timer_u32[0], (int)value_i32[1], (int)timer_u32[2], (int)value_i32[2] );
 	HAL_UART_Transmit(&huart1, (uint8_t *)DataChar, strlen(DataChar), 100);
 
 	for (int j=0; j<4; j++)
@@ -69,32 +67,47 @@ void Groza_t55_main (char* http_req_1, uint8_t* size_of_http_req )
 	HAL_Delay(500);
 	HAL_TIM_Base_Stop(&htim4);
 
-	value_i32[3] = (int)(timer_u32[0]-timer_u32[1]);
-	value_i32[4] = (int)(timer_u32[2]-timer_u32[3]);
+	value_i32[3] = timer_u32[0]-timer_u32[1];
+	value_i32[4] = timer_u32[2]-timer_u32[3];
 
-	int adc_value_U = (int)( ( ADC1_GetValue(ADC_CHANNEL_5         ) * 4 ) / 10) ;
-	int adc_value_T = (int)( 3700- ADC1_GetValue(ADC_CHANNEL_TEMPSENSOR)       ) ;
+	uint32_t adc_value_U = ( ADC1_GetValue(ADC_CHANNEL_5   ) * 4 ) / 10 ;
+	uint32_t adc_value_T = 3700- ADC1_GetValue(ADC_CHANNEL_TEMPSENSOR)  ;
 
-	sprintf(DataChar,"%d (%d)  %d (%d) ADC: %d temp: %d\r\n", (int)timer_u32[0], value_i32[3], (int)timer_u32[2], value_i32[4], adc_value_U, adc_value_T );
+	sprintf(DataChar,"%d (%d)  %d (%d) ADC: %d temp: %d\r\n",
+			(int)timer_u32[0], (int)value_i32[3], (int)timer_u32[2], (int)value_i32[4], (int)adc_value_U, (int)adc_value_T );
 	HAL_UART_Transmit(&huart1, (uint8_t *)DataChar, strlen(DataChar), 100);
 
-	sprintf(http_req_1, "GET /update?api_key=%s&field2=%d&field3=%d&field4=%d&field5=%d&field6=%d&field7=%d\r\n\r\n",
-									THINGSPEAK_API_KEY, value_i32[1], value_i32[2], value_i32[3], value_i32[4], adc_value_U, adc_value_T);
+	main_value_u32[0][circle] = value_i32[1];
+	main_value_u32[1][circle] = value_i32[2];
+	main_value_u32[2][circle] = value_i32[3];
+	main_value_u32[3][circle] = value_i32[4];
+	main_value_u32[4][circle] = adc_value_U;
+	main_value_u32[5][circle] = adc_value_T;
 
-	*size_of_http_req = strlen(http_req_1);
+	if (circle == CIRCLE_QNT-1)
+	{
+		uint32_t aver_res_u32[DEVICE_QNT];
+		for (uint8_t c = 0; c < DEVICE_QNT; c++)
+		{
+			sprintf(DataChar,"dev %d) ", (int)c );
+			HAL_UART_Transmit(&huart1, (uint8_t *)DataChar, strlen(DataChar), 100);
+
+			aver_res_u32[c] = Calc_Average(main_value_u32[c], CIRCLE_QNT);
+
+			sprintf(DataChar,"res %d\r\n", (int)aver_res_u32[c] );
+			HAL_UART_Transmit(&huart1, (uint8_t *)DataChar, strlen(DataChar), 100);
+		}
+	sprintf(http_req_1, "&field1=%d&field2=%d&field3=%d&field4=%d&field5=%d&field6=%d\r\n\r\n",
+			(int)aver_res_u32[0], (int)aver_res_u32[1], (int)aver_res_u32[2], (int)aver_res_u32[3], (int)aver_res_u32[4], (int)aver_res_u32[5] );
+	}
 }
 //*****************************************************************************
 
-int Value_T55 (uint8_t _kanal)
-{
-	return value_i32[_kanal];
-}
-
-//*****************************************************************************
 void Set_Flag_60_Sec(uint8_t _flag)
 {
 	flag_60_sec_u8 = _flag;
 }
+//*****************************************************************************
 
 uint8_t Get_Flag_60_Sec(void)
 {
